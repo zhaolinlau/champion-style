7cf402f183722a8ea9aa8f2908c318470573d31f
Zhaolin Lau <zhaolinlau@gmail.com>
2024-12-11 03:00:13 +0800

    add user role middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Http\Middleware\UserRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+	->withRouting(
+		web: __DIR__ . '/../routes/web.php',
+		commands: __DIR__ . '/../routes/console.php',
+		health: '/up',
+	)
+	->withMiddleware(function (Middleware $middleware) {
+		$middleware->alias([
+			'role' => UserRole::class
+		]);
+	})
+	->withExceptions(function (Exceptions $exceptions) {
+		//
+	})->create();
