e8617e195b99aa22854d1a7b577575aa18b7696e
Zhaolin Lau <zhaolinlau@gmail.com>
2024-12-13 23:32:51 +0800

    add contact seeder

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ContactSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void
+	{
+		$contacts = [
+			[
+				'name'	=> 'Zack',
+				'email' => 'zack@gmail.com',
+				'phone' => '0123456789',
+				'message' => 'I want to refund.'
+			],
+			[
+				'name'	=> 'William',
+				'email' => 'william@gmail.com',
+				'phone' => '0176745788',
+				'message' => 'I want to refund.'
+			],
+			[
+				'name'	=> 'Farhan',
+				'email' => 'farhan@gmail.com',
+				'phone' => '0114455324',
+				'message' => 'I want to pre-order a shampoo.'
+			],
+		];
+
+		foreach ($contacts as $contact) {
+			Contact::create($contact);
+		}
+	}
+}
