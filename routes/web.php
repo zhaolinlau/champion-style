bde6a98adde984759e4577c347d639e8f2de13be
Zhaolin Lau <zhaolinlau@gmail.com>
2024-12-14 17:28:22 +0800

    fix contact page

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+
 Route::middleware(['auth', 'role:staff'])->group(function () {
 	Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 	Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
@@ -21,5 +24,3 @@ Route::middleware(['auth', 'role:staff'])->group(function () {
 	Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
 	Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
 });
-Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
